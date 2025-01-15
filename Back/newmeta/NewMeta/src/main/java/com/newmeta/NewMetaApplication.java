5ef996823c1eb8ccbbda8e63797facd6597405dc
Junyoung_Kim <zzz5686@naver.com>
2025-01-15 12:10:28 +0900

    Back Frist 커밋

---
+++ Back/newmeta/NewMeta/src/main/java/com/newmeta/NewMetaApplication.java
@@ -0,0 +1,13 @@
+package com.newmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewMetaApplication.class, args);
+	}
+
+}
---
+++ Back/newmeta/NewMeta/src/test/java/com/newmeta/NewMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
